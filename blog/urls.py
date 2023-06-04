908c6ef268662b2dc34e30812e5b7f4b419cb7c5
tacjp9 <tacjp9@hotmail.com>
2023-06-04 12:51:33 +0900

    blog detail page is added.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+app_name = 'blog' #名前空間の設定
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent # D:\Python\pythonWs\djangogirls
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
